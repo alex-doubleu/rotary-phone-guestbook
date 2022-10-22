b3c357ec1ff416f1ae7047991b387d11df6a53db
alex-doubleu <alex.weinberg@hotmail.com>
2022-10-22 00:30:10 -0400

    Add functions for playing prompt and tone

--- src/app.py
+++ src/app.py
@@ -1,9 +1,36 @@
-import time
+from time import sleep
+import sounddevice as sd
+import soundfile as sf
+import numpy
 import logging
+import RPi.GPIO as GPIO
 
-import RPi.GPIO
-import simpleaudio as sa
+logging.basicConfig(level=logging.INFO)
 
-wave_obj = sa.WaveObject.from_wave_file("../audio/test_prompt.wav")
-play_obj = wave_obj.play()
-play_obj.wait_done()
+def play_prompt():
+    data, sample_rate = sf.read('../audio/test_prompt.wav')
+    sd.play(data, sample_rate)
+    sd.wait()
+
+def play_tone():
+    duration = 0.75
+    amplitude = 0.2
+    freq = 440.0
+    sample_rate = sd.query_devices(kind='output')['default_samplerate']
+    sample_size = numpy.arange(sample_rate * duration)
+    samples = amplitude * numpy.sin(2 * numpy.pi * sample_size * freq / sample_rate)
+
+    sd.play(samples, sample_rate)
+    sd.wait()
+
+def main():
+    logging.info('playing prompt')
+    play_prompt()
+
+    sleep(0.25)
+
+    logging.info('playing tone')
+    play_tone()
+
+if __name__ == "__main__":
+    main()
