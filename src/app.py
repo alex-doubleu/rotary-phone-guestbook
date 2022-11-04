aa0518e8b5c281b300a6c5fbf5f5ab1b2e99716e
alex-doubleu <alex.weinberg@hotmail.com>
2022-11-04 00:55:12 -0400

    Remove threading

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-import time
 import threading
 import logging
 import signal
@@ -7,7 +6,7 @@ import soundfile as sf
 import numpy
 from gpiozero import Button
 
-def callback(outdata, frames, time, status):
+def callback(outdata, frames, *_):
     global current_frame
 
     chunksize = min(len(data) - current_frame, frames)
@@ -15,25 +14,16 @@ def callback(outdata, frames, time, status):
 
     if chunksize < frames:
         outdata[chunksize:] = 0
-        event.set()
+
+        logger.info('audio stopping')
+        raise sd.CallbackStop()
 
     current_frame += chunksize
 
-def play_prompt(event):
-    logger.info('playing prompt')
-
-    with sd.OutputStream(callback=callback):
-        event.wait()
-        logger.info('event set, prompt audio stopping')
-
-    global current_frame
-    current_frame = 0
-
-def get_tone_samples():
+def get_tone_samples(sample_rate):
     duration = 0.75
     amplitude = 0.2
     freq = 440.0
-    sample_rate = sd.default.samplerate
     sample_size = numpy.arange(sample_rate * duration)
 
     silent_samples = 0 * numpy.arange(sample_rate * 0.25)
@@ -41,30 +31,18 @@ def get_tone_samples():
     
     return numpy.append(silent_samples, tone_samples).reshape(-1, 1)
 
-def record_message():
-    logger.info('recording message')
+def phone_picked_up():
+    logger.info('phone picked up')
 
-    # data = sd.rec()
-    # sd.wait()
-
-    # sf.write('../output/test_recording.wav', data, int(sample_rate))
+    stream.start()
 
 def phone_hung_up():
     logger.info('phone hung up')
 
-    event.set()
-    event.clear()
+    stream.abort()
 
-def phone_picked_up():
-    logger.info('phone picked up')
-
-    thread = threading.Thread(target=play_prompt_and_rec, args=[event])
-    thread.start()
-    # thread.join()
-
-def play_prompt_and_rec(event):
-    play_prompt(event)
-    # record_message()
+    global current_frame
+    current_frame = 0
 
 def main():
     try:
@@ -78,19 +56,15 @@ def main():
         exit()
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(levelname)s:%(asctime)s:%(message)s', level=logging.INFO)
+    logging.basicConfig(format='[%(levelname)s] %(asctime)s - %(message)s', level=logging.INFO)
     logger = logging.getLogger()
 
+    current_frame = 0
     data, sample_rate = sf.read('../audio/test_prompt.wav', always_2d=True)
 
-    input_device = sd.query_devices(kind='input')
-    output_device = sd.query_devices(kind='output')
+    # append the tone samples to the prompt audio
+    data = numpy.append(data, get_tone_samples(sample_rate), axis=0)
 
-    sd.default.device = input_device['index'], output_device['index']
-    sd.default.samplerate = sample_rate
-
-    event = threading.Event()
-    current_frame = 0
-    data = numpy.append(data, get_tone_samples(), axis=0)
+    stream = sd.OutputStream(samplerate=sample_rate, callback=callback)
 
     main()
